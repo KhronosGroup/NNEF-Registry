f93038c62f9f60f4fed51fdb6ae72ff8e7301a72
Jon Leech <oddhack@sonic.net>
2017-12-18 21:22:25 -0800

    Populate new NNEF registry with 1.0 Provisional spec

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NNEF-Registry
-Neural Network Exchange Format registry
+
+The NNEF-Registry repository contains the Neural Network Exchange Format
+specification.
+
+It is also used as a backing store for the web view of the registry at
+https://www.khronos.org/registry/NNEF/ ; commits to the master branch of
+this repository will be reflected there.
+
+Interesting files in this repository include:
+
+* index.php - toplevel index page for the web view. This relies on PHP
+  include files found elsewhere on www.khronos.org and so is not very useful
+  in isolation.
+* specs/ - NNEF specification documents.
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$static_title = 'Khronos NNEF Registry';
+
+include_once("../../assets/static_pages/khr_page_top.php");
+?>
+
+<p> The NNEF registry contains the specification of the Khronos Neural
+    Network Exchange Format. </p>
+
+<h2> <a name="specs"></a> <b>NNEF Specification</b> </h2>
+
+<p> The current version of NNEF is NNEF 1.0 Provisional. </p>
+
+<ul>
+<li> NNEF 1.0 Provisional Specification in
+     <a href="specs/1.0/nnef-1.0.pdf">PDF</a> and
+     <a href="specs/1.0/nnef-1.0.html">HTML</a> (2017-12-18). </li>
+</ul>
+
+<h6> Providing Feedback on the Registry </h6>
+
+<p> Khronos welcomes comments and issue reports. To provide feedback on the
+    NNEF registry itself (such as reporting missing content, bad links,
+    etc.), file an issue in the <a
+    href="https://github.com/KhronosGroup/NNEF-Registry/issues">
+    NNEF-Registry </a> Github project. </p>
+
+<p> For the NNEF specification, we will soon create a separate Github
+    repository for filing specification issues. </p>
+
+<?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
+</body>
+</html>
